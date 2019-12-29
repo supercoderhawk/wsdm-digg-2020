90b8e3ad114920cc0da379adb3726a5e0c6a47ba
xiayubin <xiayubin@patsnap.com>
2019-12-29 14:43:17 +0800

    update scripts

--- scripts/prepare_data.sh
+++ scripts/prepare_data.sh
@@ -2,5 +2,8 @@
 
 # transform csv data to jsonl
 python3 wsdm_digg/data_process/raw_data_formatter.py
+# split train_release.jsonl to train.jsonl and test.jsonl for experiment train and validation
+python3 wsdm_digg/data_process/data_split.py
+
 # build elasticsearch indexing
 python3 wsdm_digg/elasticsearch/indexer.py
--- scripts/train_rerank.sh
+++ scripts/train_rerank.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-EXP_NAME=''
-TRAINING_FILENAME=''
-TEST_FILENAME=''
+EXP_NAME='bert_pairwise'
+DATA_DIR=${PWD}/data/
+TRAINING_FILENAME=$DATA_DIR/train.jsonl
+TEST_FILENAME=$DATA_DIR/test.jsonl
+DEST_DIR=$DATA_DIR/rerank/
+MODEL_NAME='bert-base-uncased'
 
+export CUDA_VISIBLE_DEVICES=
 
-python3 wsdm_digg/reranking/trainer.py --
+python3 wsdm_digg/reranking/trainer.py -exp_name $EXP_NAME \
+  -train_filename $TRAINING_FILENAME -test_filename $TEST_FILENAME \
+  -dest_base_dir $DEST_DIR -model_name $MODEL_NAME

dda015c8f9eb7e848cc1bf409f5ac4d3ba08a6b7
xiayubin <xiayubin@patsnap.com>
2019-12-29 20:32:23 +0800

    implement plm rerank model

--- wsdm_digg/reranking/plm_rerank.py
+++ wsdm_digg/reranking/plm_rerank.py
@@ -1,21 +1,26 @@
 # -*- coding: UTF-8 -*-
-import argparse
 import torch
-import torch.nn  as nn
+import torch.nn as nn
+import torch.nn.functional as F
 from wsdm_digg.constants import *
 
 
 class PlmRerank(nn.Module):
-    def __init__(self):
+    def __init__(self, args):
         super().__init__()
-        self.args = self.parse_args()
+        self.args = args
         model_name = self.args.model_name
         if model_name not in MODEL_DICT:
             raise ValueError('model name is not supported.')
         model_info = MODEL_DICT[model_name]
         self.tokenizer = model_info['tokenizer_class'].from_pretrained(model_name)
         self.model = model_info['model_class'].from_pretrained(model_name)
+        self.score_proj = nn.Linear(self.args.dim_size, 1)
 
-    def forward(self, x, x_mask):
-        self.model()
-
+    def forward(self, token_ids, segment_ids, token_mask):
+        output = self.model(input_ids=token_ids,
+                            attention_mask=token_mask,
+                            token_type_ids=segment_ids)[0]
+        # scores = self.score_proj(output[:, 0])
+        scores = self.score_proj(F.tanh(output[:, 0]))
+        return scores

65430b9608a65e0098ff0ce50b43b6a309331dec
xiayubin <xiayubin@patsnap.com>
2019-12-31 16:05:04 +0800

    fix trainer

--- wsdm_digg/reranking/trainer.py
+++ wsdm_digg/reranking/trainer.py
@@ -2,8 +2,9 @@
 import argparse
 import torch
 from torch.nn import MarginRankingLoss
+from torch.utils.tensorboard import SummaryWriter
 from transformers.optimization import AdamW
-from pysenal import get_logger
+from pysenal import get_logger, write_json
 from wsdm_digg.constants import *
 from wsdm_digg.reranking.dataloader import RerankDataLoader
 from wsdm_digg.reranking.plm_rerank import PlmRerank
@@ -14,17 +15,26 @@ class PlmTrainer(object):
         self.args = self.parse_args()
         self.model_name = self.args.model_name
         self.model_info = MODEL_DICT[self.model_name]
-        self.tokenizer = self.model_info['tokenizer_class'].from_pretrained(self.model_name)
+        if 'path' in self.model_info:
+            tokenizer_path = self.model_info['path'] + 'vocab.txt'
+        else:
+            tokenizer_path = self.model_name
+        self.tokenizer = self.model_info['tokenizer_class'].from_pretrained(tokenizer_path)
         self.train_loader = RerankDataLoader(self.args.train_filename,
                                              self.tokenizer,
-                                             self.args)
+                                             self.args,
+                                             'train')
         self.logger = get_logger('rerank_trainer')
+        logdir = self.args.dest_base_dir + 'logs/'
+        if not os.path.exists(logdir):
+            os.mkdir(logdir)
+        self.writer = SummaryWriter(logdir)
 
     def train(self):
         try:
             self.train_func()
-        except:
-            pass
+        except Exception as e:
+            raise e
         finally:
             del self.train_loader
 
@@ -32,6 +42,8 @@ class PlmTrainer(object):
         step = 0
         bert_lr = 1e-5
         model = PlmRerank(self.args)
+        if torch.cuda.is_available():
+            model.cuda()
         loss_fct = MarginRankingLoss(margin=1, reduction='mean')
         optimizer = AdamW(model.parameters(), lr=bert_lr)
         for epoch in range(1, self.args.epoch + 1):
@@ -49,12 +61,15 @@ class PlmTrainer(object):
                 if torch.cuda.is_available():
                     y = y.cuda()
                 loss = loss_fct(pos_scores, neg_scores, y)
-                # self.logger.info("+ve scores : %r" % pos_scores)
-                # self.logger.info("-ve scores : %r" % neg_scores)
-                # self.logger.info("Train step loss : %0.5f" % loss)
+                self.writer.add_scalar('loss', loss, step)
                 loss.backward()
                 optimizer.step()
                 step += 1
+                if step % self.args.save_model_step == 0:
+                    model_basename = self.args.dest_base_dir + self.args.exp_name
+                    model_basename += '_epoch_{}_step_{}'.format(epoch, step)
+                    torch.save(model.state_dict(), model_basename + '.model')
+                    write_json(model_basename + '.json', vars(self.args))
 
     def parse_args(self, args=None):
         parser = argparse.ArgumentParser()
@@ -65,7 +80,8 @@ class PlmTrainer(object):
         parser.add_argument("-batch_size", type=int, default=4, help='')
         parser.add_argument("-max_len", type=int, default=512, help='')
         parser.add_argument("-dim_size", type=int, default=768, help='')
-        parser.add_argument("-epoch", type=int, default=10, help='')
+        parser.add_argument("-epoch", type=int, default=5, help='')
+        parser.add_argument("-save_model_step", type=int, default=2000, help='')
 
         # model parameter
         parser.add_argument("-model_name", required=True, type=str, help='')

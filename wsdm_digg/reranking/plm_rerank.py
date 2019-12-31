765cc50c8abff35d4500a7031d0dd02e3e13c44e
xiayubin <xiayubin@patsnap.com>
2019-12-31 16:04:16 +0800

    fix model

--- wsdm_digg/reranking/plm_rerank.py
+++ wsdm_digg/reranking/plm_rerank.py
@@ -13,14 +13,18 @@ class PlmRerank(nn.Module):
         if model_name not in MODEL_DICT:
             raise ValueError('model name is not supported.')
         model_info = MODEL_DICT[model_name]
-        self.tokenizer = model_info['tokenizer_class'].from_pretrained(model_name)
+        # self.tokenizer = model_info['tokenizer_class'].from_pretrained(model_name)
+        if 'path' in model_info:
+            model_name = model_info['path']
         self.model = model_info['model_class'].from_pretrained(model_name)
+        if torch.cuda.is_available():
+            self.model.cuda()
         self.score_proj = nn.Linear(self.args.dim_size, 1)
 
     def forward(self, token_ids, segment_ids, token_mask):
         output = self.model(input_ids=token_ids,
                             attention_mask=token_mask,
                             token_type_ids=segment_ids)[0]
-        # scores = self.score_proj(output[:, 0])
-        scores = self.score_proj(F.tanh(output[:, 0]))
+        scores = self.score_proj(output[:, 0])
+        # scores = self.score_proj(F.tanh(output[:, 0]))
         return scores

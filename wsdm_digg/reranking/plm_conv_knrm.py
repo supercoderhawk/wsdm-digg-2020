2e84e34d3a1cc8429a885fe00f9b416d9bc9b8f1
xiayubin <xiayubin@patsnap.com>
2020-01-04 21:31:07 +0800

    fix conv_knrm

--- wsdm_digg/reranking/plm_conv_knrm.py
+++ wsdm_digg/reranking/plm_conv_knrm.py
@@ -16,29 +16,43 @@ class PlmConvKnrm(nn.Module):
         model_info = MODEL_DICT[plm_model_name]
         if 'path' in model_info:
             plm_model_name = model_info['path']
-        self.model = model_info['model_class'].from_pretrained(plm_model_name)
+        self.plm_model = model_info['model_class'].from_pretrained(plm_model_name)
         if torch.cuda.is_available():
-            self.model.cuda()
+            self.plm_model.cuda()
         self.mean_list = self.args.mean_list
         self.stddev_list = self.args.stddev_list
         assert len(self.mean_list) == len(self.stddev_list)
         self.conv_list = nn.ModuleList()
         self.window_count = len(self.args.window_size_list)
         for window in self.args.window_size_list:
-            conv = nn.Conv1d(in_channels=1,
+            conv = nn.Conv1d(in_channels=self.args.dim_size,
                              out_channels=self.args.filter_size,
                              kernel_size=window)
             self.conv_list.append(conv)
         self.query_max_len = self.args.query_max_len
         self.doc_max_len = self.args.max_len - self.query_max_len - self.args.special_token_count
-        self.score_proj = nn.Linear(len(self.mean_list), 1, bias=True)
+        self.use_context_vector = self.args.use_context_vector
+        self.context_merge_method = self.args.context_merge_method
+        feature_size = len(self.mean_list) * self.window_count ** 2
+        if self.use_context_vector:
+            if self.context_merge_method == 'vector_concat':
+                self.score_dim = feature_size + self.args.dim_size
+            elif self.context_merge_method == 'score_add':
+                self.score_dim = feature_size
+                self.context_score_proj = nn.Linear(self.args.dim_size, 1, bias=True)
+            else:
+                raise ValueError('error...')
+        else:
+            self.score_dim = feature_size
+        self.score_proj = nn.Linear(self.score_dim, 1, bias=True)
+        # self.score_proj = nn.Linear(len(self.mean_list), 1, bias=True)
 
     def forward(self, token_ids, segment_ids, token_mask, query_lens, doc_lens):
-        contextualized_embed = self.model(input_ids=token_ids,
-                                          attention_mask=token_mask,
-                                          token_type_ids=segment_ids)[0]
-        query_embed = contextualized_embed[:, 1:self.query_max_len]
-        doc_start_idx = self.query_max_len + 1
+        contextualized_embed = self.plm_model(input_ids=token_ids,
+                                              attention_mask=token_mask,
+                                              token_type_ids=segment_ids)[0]
+        query_embed = contextualized_embed[:, 1:self.query_max_len + 1]
+        doc_start_idx = self.query_max_len + 2
         doc_end_idx = doc_start_idx + self.doc_max_len
         doc_embed = contextualized_embed[:, doc_start_idx:doc_end_idx]
         context_vector = contextualized_embed[:, 0]
@@ -50,12 +64,12 @@ class PlmConvKnrm(nn.Module):
                 q_conv = q_conv_list[q_idx]
                 d_conv = d_conv_list[d_idx]
                 sim_matrix = self.get_similarity_matrix(q_conv, d_conv, query_lens, doc_lens)
-                rbf_feature = self.get_rbf_feature(sim_matrix, context_vector)
+                rbf_feature = self.get_rbf_feature(sim_matrix)
                 if total_rbf_feature is None:
                     total_rbf_feature = rbf_feature
                 else:
                     total_rbf_feature = torch.cat([total_rbf_feature, rbf_feature], dim=1)
-        score = self.get_rank_score(total_rbf_feature)
+        score = self.get_rank_score(total_rbf_feature, context_vector)
         return score
 
     def get_conv_matrix(self, query_embed, doc_embed, query_lens, doc_lens):
@@ -70,29 +84,29 @@ class PlmConvKnrm(nn.Module):
         return query_conv_result, doc_conv_result
 
     def get_similarity_matrix(self, query_embed, doc_embed, query_lens, doc_lens):
-        batch_size = query_lens.size(0)
+        # batch_size = query_lens.size(0)
         query_sum = torch.sqrt(torch.sum(query_embed ** 2, dim=2).unsqueeze(2))
         doc_sum = torch.sqrt(torch.sum(doc_embed ** 2, dim=2).unsqueeze(1))
 
         sim_matrix = torch.bmm(query_embed, doc_embed.permute(0, 2, 1)) / torch.bmm(query_sum, doc_sum)
         # mask position with pad char in query and doc to value 0
-        q_max_len = self.query_max_len
-        d_max_len = self.doc_max_len
-        q_range = torch.arange(q_max_len).unsqueeze(0).repeat(batch_size, 1)
-        if torch.cuda.is_available():
-            q_range = q_range.cuda()
-        q_mask = q_range <= query_lens.unsqueeze(1)
-        q_mask = q_mask.unsqueeze(2).repeat(1, 1, d_max_len)
-        d_range = torch.arange(d_max_len).unsqueeze(0).repeat(batch_size, 1)
-        if torch.cuda.is_available():
-            d_range = d_range.cuda()
-        d_mask = d_range <= doc_lens.unsqueeze(1)
-        d_mask = d_mask.unsqueeze(1).repeat(1, q_max_len, 1)
-        total_mask = ~(q_mask * d_mask)
-        sim_matrix.masked_fill_(total_mask, 0.0)
+        # q_max_len = self.query_max_len
+        # d_max_len = self.doc_max_len
+        # q_range = torch.arange(q_max_len).unsqueeze(0).repeat(batch_size, 1)
+        # if torch.cuda.is_available():
+        #     q_range = q_range.cuda()
+        # q_mask = q_range <= query_lens.unsqueeze(1)
+        # q_mask = q_mask.unsqueeze(2).repeat(1, 1, d_max_len)
+        # d_range = torch.arange(d_max_len).unsqueeze(0).repeat(batch_size, 1)
+        # if torch.cuda.is_available():
+        #     d_range = d_range.cuda()
+        # d_mask = d_range <= doc_lens.unsqueeze(1)
+        # d_mask = d_mask.unsqueeze(1).repeat(1, q_max_len, 1)
+        # total_mask = ~(q_mask * d_mask)
+        # sim_matrix.masked_fill_(total_mask, 0.0)
         return sim_matrix
 
-    def get_rbf_feature(self, similarity_matrix, context_vector):
+    def get_rbf_feature(self, similarity_matrix):
         rbf_feature = None
         for mean, stddev in zip(self.mean_list, self.stddev_list):
             rbf_matrix = torch.exp(-(similarity_matrix - mean) ** 2 / (2 * stddev ** 2))
@@ -101,9 +115,20 @@ class PlmConvKnrm(nn.Module):
             else:
                 rbf_feature = torch.cat((rbf_feature, torch.sum(rbf_matrix, dim=2).unsqueeze(1)), dim=1)
         rbf_feature = torch.sum(torch.log(rbf_feature), dim=2)
-        if self.use_context_vector:
-            rbf_feature = torch.cat((rbf_feature, context_vector), dim=1)
+        # if self.use_context_vector:
+        #     rbf_feature = torch.cat((rbf_feature, context_vector), dim=1)
         return rbf_feature
 
-    def get_rank_score(self, rbf_feature):
-        return torch.tanh(self.score_proj(rbf_feature))
+    def get_rank_score(self, rbf_feature, context_feature):
+        if self.use_context_vector:
+            if self.context_merge_method == 'vector_concat':
+                rbf_feature = torch.cat((rbf_feature, context_feature), dim=1)
+                score = self.score_proj(rbf_feature)
+            elif self.context_merge_method == 'score_add':
+                score = self.score_proj(rbf_feature) + self.context_score_proj(context_feature)
+            else:
+                raise ValueError('mode error')
+            # score =
+        else:
+            score = self.score_proj(rbf_feature)
+        return score

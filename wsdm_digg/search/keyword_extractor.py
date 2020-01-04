d1e71b504ec3fb6ff7b7c74632a66e3cf42a9c86
xiayubin <xiayubin@patsnap.com>
2020-01-04 21:32:28 +0800

    lemmatize the word and noun chunk

--- wsdm_digg/search/keyword_extractor.py
+++ wsdm_digg/search/keyword_extractor.py
@@ -1,4 +1,5 @@
 # -*- coding: UTF-8 -*-
+import re
 import spacy
 from textacy.keyterms import key_terms_from_semantic_network
 
@@ -7,11 +8,11 @@ class KeywordExtractor(object):
     nlp = spacy.load('en', disable=['ner', 'parser', 'textcat'])
     chunk_nlp = spacy.load('en', disable=['ner', 'textcat'])
     noun_chunk_stopwords = ('their ', 'the ', 'our ', 'my ', 'a ', 'an ', 'many ')
-
+    es_special_char_regex = re.compile(r'(?P<PUNC>[+-=&|!(){}\[\]^"~*?:/])')
     def __init__(self):
         self.nlp.add_pipe(self.nlp.create_pipe('sentencizer'))
 
-    def textrank(self, input_data, topk,window_size, edge_weighting):
+    def textrank(self, input_data, topk, window_size, edge_weighting):
         if isinstance(input_data, spacy.tokens.Doc):
             doc = input_data
         elif isinstance(input_data, str):
@@ -23,13 +24,19 @@ class KeywordExtractor(object):
                                                        edge_weighting=edge_weighting,
                                                        join_key_words=False,
                                                        n_keyterms=topk)
-        result_terms = [term for term, score in textrank_ret]
+        result_terms = []
+        for term, score in textrank_ret:
+            if ' ' in term:
+                result_terms.append('"' + self.format_term(term) + '"')
+            else:
+                result_terms.append(self.format_term(term))
+        # result_terms = [term for term, score in textrank_ret]
         return result_terms
 
     def get_noun_chunk(self, text):
         noun_chunk_list = []
         for noun_chunk in self.chunk_nlp(text).noun_chunks:
-            noun_chunk_list.append(noun_chunk.text)
+            noun_chunk_list.append(noun_chunk.lemma_)
         noun_chunk_list = self.noun_chunk_post_process(noun_chunk_list)
         return noun_chunk_list
 
@@ -52,9 +59,9 @@ class KeywordExtractor(object):
             if not token.is_stop and not token.is_punct:
                 if pos:
                     if token.pos_ in pos_rules:
-                        query_words.append(token.text)
+                        query_words.append(token.lemma_)
                 else:
-                    query_words.append(token.text)
+                    query_words.append(token.lemma_)
         return self.term_dedupe(query_words)
 
     def term_dedupe(self, terms):
@@ -63,3 +70,13 @@ class KeywordExtractor(object):
             if term not in result_terms:
                 result_terms.append(term)
         return result_terms
+
+    def format_term(self, term):
+        return self.es_special_char_regex.sub(r'\\\g<PUNC>', term)
+
+    def format_terms(self, term_list):
+        formatted_term_list = []
+        for term in term_list:
+            new_term = self.es_special_char_regex.sub(r'\\\g<PUNC>', term)
+            formatted_term_list.append(new_term)
+        return formatted_term_list

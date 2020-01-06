9213ef881f0e6565de66ec4941a6efeaf134898b
xiayubin <xiayubin@patsnap.com>
2020-01-06 20:10:45 +0800

    scale output probability

--- wsdm_digg/reranking/plm_rerank.py
+++ wsdm_digg/reranking/plm_rerank.py
@@ -10,20 +10,20 @@ class PlmRerank(nn.Module):
     def __init__(self, args):
         super().__init__()
         self.args = args
-        model_name = self.args.model_name
-        if model_name not in MODEL_DICT:
+        plm_model_name = self.args.plm_model_name
+        if plm_model_name not in MODEL_DICT:
             raise ValueError('model name is not supported.')
-        model_info = MODEL_DICT[model_name]
+        model_info = MODEL_DICT[plm_model_name]
         if 'path' in model_info:
-            model_name = model_info['path']
-        self.model = model_info['model_class'].from_pretrained(model_name)
+            plm_model_name = model_info['path']
+        self.plm_model = model_info['model_class'].from_pretrained(plm_model_name)
         if torch.cuda.is_available():
-            self.model.cuda()
+            self.plm_model.cuda()
         self.score_proj = nn.Linear(self.args.dim_size, 1)
 
-    def forward(self, token_ids, segment_ids, token_mask):
-        output = self.model(input_ids=token_ids,
-                            attention_mask=token_mask,
-                            token_type_ids=segment_ids)[0]
-        scores = self.score_proj(output[:, 0]).squeeze(1)
+    def forward(self, token_ids, segment_ids, token_mask, query_lens, doc_lens):
+        output = self.plm_model(input_ids=token_ids,
+                                attention_mask=token_mask,
+                                token_type_ids=segment_ids)[0]
+        scores = torch.tanh(self.score_proj(output[:, 0]))
         return scores

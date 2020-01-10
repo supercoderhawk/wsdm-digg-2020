df0376bbcf9f2f5854d9d659c97fe58c833ad2d0
xiayubin <xiayubin@patsnap.com>
2020-01-10 19:47:14 +0800

    add plm model loader method

--- wsdm_digg/reranking/model_loader.py
+++ wsdm_digg/reranking/model_loader.py
@@ -1,4 +1,5 @@
 # -*- coding: UTF-8 -*-
+import torch
 from wsdm_digg.reranking.plm_rerank import PlmRerank
 from wsdm_digg.reranking.plm_knrm import PlmKnrm
 from wsdm_digg.reranking.plm_conv_knrm import PlmConvKnrm
@@ -6,7 +7,7 @@ from wsdm_digg.reranking.plm_conv_knrm import PlmConvKnrm
 _MODEL_NAME_SET = {'plm', 'knrm', 'conv-knrm'}
 
 
-def load_model(args):
+def load_rerank_model(args):
     model_name = args.rerank_model_name
     if model_name not in _MODEL_NAME_SET:
         raise ValueError('model name {} is not implemented'.format(model_name))
@@ -19,7 +20,7 @@ def load_model(args):
     return model
 
 
-def get_score_func(model, prefix=None):
+def get_score_func(model, prefix=None, inference=False):
     def calculate(batch):
         if prefix:
             token_field = '{}_token'.format(prefix)
@@ -33,11 +34,20 @@ def get_score_func(model, prefix=None):
             mask_field = 'mask'
             query_lens_field = 'query_lens'
             doc_lens_field = 'doc_lens'
-        scores = model(token_ids=batch[token_field],
-                       segment_ids=batch[segment_field],
-                       token_mask=batch[mask_field],
-                       query_lens=batch[query_lens_field],
-                       doc_lens=batch[doc_lens_field])
+        if inference:
+            with torch.no_grad():
+                scores = model(token_ids=batch[token_field],
+                               segment_ids=batch[segment_field],
+                               token_mask=batch[mask_field],
+                               query_lens=batch[query_lens_field],
+                               doc_lens=batch[doc_lens_field])
+                scores = scores.squeeze(1)
+        else:
+            scores = model(token_ids=batch[token_field],
+                           segment_ids=batch[segment_field],
+                           token_mask=batch[mask_field],
+                           query_lens=batch[query_lens_field],
+                           doc_lens=batch[doc_lens_field])
         return scores
 
     return calculate
